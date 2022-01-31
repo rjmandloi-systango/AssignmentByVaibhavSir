f0a5658a2c1848abe6c60117c21a71142239cb97
rjmandloi-systango <rjmandloi@isystango.com>
2022-01-31 12:43:02 +0530

    add questuion no 19

---
+++ program19/19.js
@@ -0,0 +1,8 @@
+function hoverDemo()
+{
+let colors=["red","green","yellow","blue","orange"];
+let number=Math.floor(Math.random()*6);
+let newColor=colors[number];
+console.log(newColor)
+para.style.color=newColor;
+}

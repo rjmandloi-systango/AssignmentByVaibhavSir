6ad7fdf33acdaa332a92d90f2314979b0633804b
rjmandloi-systango <rjmandloi@isystango.com>
2022-01-31 14:27:05 +0530

    add questuion no 11b

---
+++ program11b.js
@@ -0,0 +1,13 @@
+function longestWord(string) {
+    var str = string.split(" ");
+    var longest = 0;
+    var word = null;
+    for (var i = 0; i < str.length; i++) {
+        if (longest < str[i].length) {
+            longest = str[i].length;
+            word = str[i];
+        }
+    }
+    return word;
+}
+console.log(longestWord("i am rajat defhjdebfhdj ddgfohiewgrehgueir"));
